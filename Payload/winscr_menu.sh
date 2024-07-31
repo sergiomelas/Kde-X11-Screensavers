21f62b190d2bb65a65bd2ddf2c9c71b1ed078c51
Sergio Melas <sergiomelas@gmail.com>
2024-07-31 13:26:56 +0200

    V 2.1
    Corrected bug with system lockscreen, added memory of lockscreen
    
    Thx to Christitus for the inspirational post : https://www.youtube.com/watch?v=J2zasJz5vuA&t=384s
    on youtube and the info below I used to create this
    
    https://christitus.com/lcars-screensaver/

--- Payload/winscr_menu.sh
+++ Payload/winscr_menu.sh
@@ -21,31 +21,31 @@ Choice="Choose Screensaver"
 echo  ""
 echo -n "Plese choose an option"
 
-Choice=$(zenity --list  --title="Win Screensavers Menu" --text "Pick an option" --column "Pick" --column "Answer" --radiolist  FALSE "Choose Screensaver" FALSE "Test Screensaver"  FALSE "Configure Screensaver"  FALSE "Configure Timeout"  FALSE "About"  )
+Choice=$(zenity --list  --title="Win Screensavers Menu" --text "Pick an option" --column "Pick" --column "Answer"  --radiolist  FALSE "Choose Screensaver" FALSE "Test Screensaver"  FALSE "Configure Screensaver"  FALSE "Configure Timeout"  FALSE "About"  )
 
 #process menu choice
 case $Choice in
-'Choose Screensaver')
+  'Choose Screensaver')
     echo  "Choose Screensaver"
     cmd="/home/$USER/.winscr/winscr_choose.sh"
     kstart5 bash $cmd  &
   ;;
-'Test Screensaver')
+  'Test Screensaver')
     echo  "Test Screensaver"
     cmd="/home/$USER/.winscr/winscr_test.sh"
     kstart5 bash $cmd  &
   ;;
-'Configure Screensaver')
+  'Configure Screensaver')
     echo  "Configure Screensaver"
     cmd="/home/$USER/.winscr/winscr_configure.sh"
     kstart5 bash $cmd  &
   ;;
-'Configure Timout')
+  'Configure Timeout')
     echo  "Configure Timout"
     cmd="/home/$USER/.winscr/winscr_timeout.sh"
     kstart5 bash  $cmd  &
- ;;
- 'About')
+  ;;
+  'About')
     echo  "About SCreen"
     cmd="/home/$USER/.winscr/winscr_about.sh"
     kstart5 bash  $cmd  &
